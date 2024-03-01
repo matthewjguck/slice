94e6d529f1f7e304c523a6f211f0d3e0ece3d55d
matthewjguck <160309784+matthewjguck@users.noreply.github.com>
2024-03-01 03:14:54 -0800

    Initial Commit

---
+++ slice.py
@@ -0,0 +1,8 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+import sys
+
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry 
